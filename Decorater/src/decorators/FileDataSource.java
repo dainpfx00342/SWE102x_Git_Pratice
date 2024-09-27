5f44235c4de18fe628242d824d58ef77eaa7bea8
dainpfx00342 <dainpfx00342@funix.edu.vn>
2024-09-27 23:26:53 +0700

    adsd

---
+++ Decorater/src/decorators/FileDataSource.java
@@ -0,0 +1,43 @@
+package decorators;
+
+import java.io.*;
+
+public class FileDataSource implements DataSource {
+	private String name;
+	private File file;
+
+	public FileDataSource(String name) {
+		this.name = name;
+
+		file = new File(name);
+		try {
+			file.getParentFile().mkdirs();
+			file.createNewFile();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	@Override
+	public void writeData(String data) {
+		try (OutputStream fos = new FileOutputStream(file, false)) {
+			fos.write(data.getBytes(), 0, data.length());
+		} catch (IOException ex) {
+			System.out.println(ex.getMessage());
+		}
+	}
+
+	@Override
+	public String readData() {
+		char[] buffer = null;
+		File file = new File(name);
+		try (FileReader reader = new FileReader(file)) {
+			buffer = new char[(int) file.length()];
+			reader.read(buffer);
+		} catch (IOException ex) {
+			System.out.println(ex.getMessage());
+		}
+		return new String(buffer);
+	}
+}
